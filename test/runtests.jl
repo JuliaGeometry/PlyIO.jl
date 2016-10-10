5d55ab39cfea8d491b6875c679638abd98dbc977
Chris Foster <chris42f@gmail.com>
2016-10-10 23:46:59 +1000

    Add test for super simple ascii ply file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,41 @@ using Base.Test
 
 import PlyIO: load_ply, save_ply, Ply, Element, ArrayProperty, ListProperty
 
+@testset "simple" begin
+    ply = Ply()
+
+    push!(ply, Element("A",
+                       ArrayProperty("x", UInt8[1,2,3]),
+                       ListProperty("y", [[0,1], [2,3,4], [5]])))
+    push!(ply, Element("B",
+                       ArrayProperty("y", Int16[-1,1])))
+
+    buf = IOBuffer()
+    save_ply(ply, buf, ascii=true)
+    str = takebuf_string(buf)
+    open("foo.ply", "w") do fid
+        write(fid, str)
+    end
+    @test str ==
+    """
+    ply
+    format ascii 1.0
+    element A 3
+    property uchar x
+    property list int int64 y
+    element B 2
+    property short y
+    end_header
+    1 2 0 1  
+    2 3 2 3 4  
+    3 1 5  
+    -1 
+    1 
+    """
+end
+
 @testset "roundtrip" begin
-    @testset "ascii=$test_ascii" for test_ascii in [true, false]
+    @testset "ascii=$test_ascii" for test_ascii in [false, true]
         ply = Ply()
 
         nverts = 10
